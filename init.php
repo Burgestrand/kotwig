537637d6e0ed269fb80c15cfad46a1863dbb68a6
Kim Burgestrand <kim@burgestrand.se>
2010-03-18 00:08:11 +0100

    Add loading of twig in init

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+    
+    require_once Kohana::find_file('vendor', 'Twig/lib/Twig/Autoloader');
+    Twig_Autoloader::register();
+    
+/* End of file init.php */
+/* Location: ./init.php */ 
